0416825fac0008e8e625c55b76d1847b964f4571
AmerAmora <Amer.m.amora@gmail.com>
2022-12-13 16:58:29 +0300

    Upload

--- 13-12/Task1.sql
+++ 13-12/Task1.sql
@@ -9,20 +9,15 @@ CREATE TABLE customers (
 	state VARCHAR (25),
 	zip_code VARCHAR (5)
 );
+insert into customers values('Amer','Amora','0789949499','Amer.m.amora@gmail.com','Palestine','irbid','Irbid','55555' );
 CREATE TABLE orders (
 	order_id INT IDENTITY (1, 1) PRIMARY KEY,
 	customer_id INT,
-	order_status tinyint NOT NULL,
 	order_date DATE NOT NULL,
-	required_date DATE NOT NULL,
-	shipped_date DATE,
-	store_id INT NOT NULL,
-	staff_id INT NOT NULL,
 	FOREIGN KEY (customer_id) 
-        REFERENCES customers (customer_id) 
-        ON DELETE CASCADE ON UPDATE CASCADE
-	
+        REFERENCES customers (customer_id) 	
 );
+insert into orders values(1,'12-13-2022');
 CREATE TABLE products (
 	product_id INT IDENTITY (1, 1) PRIMARY KEY,
 	product_name VARCHAR (255) NOT NULL,
@@ -30,7 +25,11 @@ CREATE TABLE products (
 	list_price DECIMAL (10, 2) NOT NULL,
 	
 );
+insert into products values('shirt',2022,200);
+insert into products values('pants',2021,100);
+insert into products values('Accessories',2020,50);
 create table product_order(
+product_order_id INT IDENTITY (1, 1) PRIMARY KEY,
 product_id int,
 order_id int,
 FOREIGN KEY (product_id) 
@@ -38,6 +37,13 @@ FOREIGN KEY (product_id)
 		FOREIGN KEY (order_id) 
         REFERENCES orders (order_id) 
 );
+insert into product_order values(1,2);
+insert into product_order values(2,2);
+insert into product_order values(3,2);
+
+select * from product_order;
+select product_order.product_id,products.product_name,product_order.order_id
+from product_order inner join products on product_order.product_id=products.product_id;
 create table Employee(
 employeeId int  ,
 firstName varchar(50), 
