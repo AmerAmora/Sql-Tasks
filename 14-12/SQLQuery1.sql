d0864ef2b91bbf319dbc79c83e3f2274edf84284
unknown <amer.m.amora@gmail.com>
2022-12-14 21:10:20 +0300

    Uploading task1

---
+++ 14-12/SQLQuery1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers (
+	customer_id INT IDENTITY (1, 1) PRIMARY KEY,
+	first_name VARCHAR (255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL,
+	phone VARCHAR (25),
+	email VARCHAR (255) NOT NULL,
+	user_image varchar(255)
+);
+select * from customers;
